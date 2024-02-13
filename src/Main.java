e46c511c47fc7e9f5bd6280f588503bdb7d4e7ad
Alexander7145 <aberofeevy@gmail.com>
2024-02-13 16:46:49 +0300

    Циклы, начало.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,71 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(" Висококосный год " + i);
+        }
+
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        for (int i = 2; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+        int saved = 29000;
+        int deposit = 0;
+        for (int i = 1; i <= 12; i++) {
+            deposit = deposit + saved;
+            System.out.println(" Месяц " + i + " сумма накоплений равна " + deposit + " рублей ");
+            {
+                deposit = deposit + saved;
+                deposit = deposit + deposit / 100;
+                System.out.println(" Месяц " + i + " сумма накоплений равна " + deposit + " рублей ");
+            }
+
+        }
+        for (int i = 1; i <=10 ; i ++)
+            System.out.println("2 * " + i + " = " + 2 * i);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
