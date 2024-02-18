73b6576e34424663a2cdae28fad21b9515f25f0b
Alexander7145 <aberofeevy@gmail.com>
2024-02-18 11:44:15 +0300

    Циклы, часть 2.

--- src/Main.java
+++ src/Main.java
@@ -2,51 +2,125 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-        for (int i = 10; i > 0; i--) {
-            System.out.println(i);
-        }
 
-        for (int i = 0; i < 17; i = i + 2) {
-            System.out.println(i);
+        int salary = 15000;
+        int total = 0;
+        int i = 0;
+        while (total <= 2_459_000) {
+            total = total + salary;
+            total = total + total / 100;
+            i = i + 1;
+            System.out.println(" Месяц " + i + " сумма накоплений равна " + total + " рублей ");
         }
-        for (int i = 10; i >= -10; i--) {
-            System.out.println(i);
+        System.out.println();
+        int a = 0;
+        while (a < 10) {
+            a = a + 1;
+            System.out.print(" " + a);
         }
-        for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println(" Висококосный год " + i);
+        System.out.println();
+        int start = 11;
+        for (; start > 1; ) {
+            start = start - 1;
+            System.out.print(" " + start);
         }
-
-        for (int i = 7; i <= 98; i = i + 7) {
-            System.out.println(i);
+        System.out.println();
+        System.out.println();
+        int years = 10;
+        int population = 12_000_000;
+        int birthRate = population * 17 / 1000;
+        int deathRate = population * 8 / 1000;
+        for (; i < 10; ++i) {
+            population = population + birthRate - deathRate;
+            System.out.println(" Год " + (i + 1) + " население " + population);
         }
-        for (int i = 2; i <= 512; i = i * 2) {
-            System.out.println(i);
+        System.out.println();
+        int deposit = 15000;
+        int capital = 12_000_000;
+        int monthNumber = 0;
+        while (deposit < capital) {
+            deposit *= 1.07;
+            ++monthNumber;
+            System.out.println(" Месяц = " + monthNumber + " , сумма накоплений = " + deposit);
         }
-        int saved = 29000;
-        int vbanku = 0;
-        for (int i = 1; i <= 12; i++) {
-            vbanku = vbanku + saved;
-            System.out.println(" Месяц " + i + " сумма накоплений равна " + vbanku + " рублей ");}
-
-        int stored = 29000;
-        int deposit = 0;
-        for (int i = 1; i <=12; i++){
-            deposit = deposit + deposit/100;
-            deposit = deposit + stored;
-            System.out.println(" Месяц " + i + " сумма накоплений равна " + deposit + " рублей ");
+        System.out.println();
+        int money = 15000;
+        int quantity = 12_000_000;
+        int month = 0;
+        while (money < quantity) {
+            money *= 1.07;
+            ++month;
+            if (month % 6 == 0) {
+                System.out.println(" Месяц = " + month + " , сумма накоплений = " + money);
+            }
         }
-
-
-
-        for (int i = 1; i <=10 ; i ++)
-            System.out.println("2 * " + i + " = " + 2 * i);
-
+        System.out.println();
+        int startMoney = 15000;
+        int monthCount = 9 * 12;
+        for (int b = 1; b <= monthCount; b++) {
+            startMoney *= 1.07;
+            if (b % 6 == 0) {
+                System.out.println(" Месяц " + b + " накопления " + startMoney);
+            }
+        }
+        System.out.println();
+        for (int friDay = 4; friDay <= 31; friDay = friDay + 7 ) {
+            System.out.println(" Сегодня пятница " + friDay + "  число. Необходимо подготовить отчет ");
+        }
+        System.out.println();
+        int currentYear = 2024;
+        int startComet = currentYear - 200;
+        int finalComet = currentYear + 100;
+        for (int b = startComet; b <= finalComet; b ++){
+            if (b % 79 == 0)
+                System.out.println( " Появление кометы в " + b );
+        }
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
 
 
 
